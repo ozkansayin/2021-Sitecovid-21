152a62d400a1875eaf74ed8cc4ed73f95aaa352b
Ozkan Sayin <osayin@reference.be>
2021-03-07 01:05:22 +0300

    Remove unnecessary using

--- src/Feature/SmartNavigation/code/Repositories/EntryRepository.cs
+++ src/Feature/SmartNavigation/code/Repositories/EntryRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using Feature.SmartNavigation.Configuration;
 using Feature.SmartNavigation.Models;
 using LiteDB;
